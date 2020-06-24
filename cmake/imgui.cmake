c44e24c97433d5c4f239f6b877f4b3d1b25fe02c
Austin <austin@jinja.dev>
2020-06-24 00:03:10 -0400

    Updated openvr, added imgui and imgui.cmake

---
+++ cmake/imgui.cmake
@@ -0,0 +1,4 @@
+set(IMGUI_SRCS
+	"${PROJECT_SOURCE_DIR}/extern/imgui/src/imgui.cpp"
+	"${PROJECT_SOURCE_DIR}/extern/imgui/src/imgui.h")
+set(IMGUI_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/extern/imgui/src/")
